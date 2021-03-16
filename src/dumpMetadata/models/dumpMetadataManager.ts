222e53e1666646889ef9708a8b6fb7ac7afd4157
amito <samit2310@gmail.com>
2021-03-16 14:52:34 +0200

    feat(helm): added helm chart for k8s deployment

--- src/dumpMetadata/models/dumpMetadataManager.ts
+++ src/dumpMetadata/models/dumpMetadataManager.ts
@@ -35,8 +35,8 @@ export class DumpMetadataManager {
   }
 
   private getUrlHeader(): string {
-    const { protocol, host } = this.objectStorageConfig;
-    return `${protocol}://${host}`;
+    const { protocol, host, port } = this.objectStorageConfig;
+    return `${protocol}://${host}:${port}`;
   }
 
   private convertDumpMetadataToDumpMetadataResponse(dumpMetadata: IDumpMetadata): DumpMetadataResponse {
--- tests/helpers/index.ts
+++ tests/helpers/index.ts
@@ -11,6 +11,7 @@ interface IntegrationDumpMetadataResponse extends Omit<DumpMetadataResponse, 'ti
 export const mockObjectStorageConfig = {
   protocol: 'http',
   host: 'some_storage_host',
+  port: '9000',
 };
 
 export const DEFAULT_SORT = 'desc';
@@ -45,8 +46,8 @@ export const createMultipleFakeDumpsMetadata = (amount: number): IDumpMetadata[]
 
 export const convertFakeToResponse = (fakeDumpMetadata: IDumpMetadata): DumpMetadataResponse => {
   const { bucket, ...restOfMetadata } = fakeDumpMetadata;
-  const { protocol, host } = mockObjectStorageConfig;
-  return { ...restOfMetadata, url: `${protocol}://${host}/${bucket}/${restOfMetadata.name}` };
+  const { protocol, host, port } = mockObjectStorageConfig;
+  return { ...restOfMetadata, url: `${protocol}://${host}:${port}/${bucket}/${restOfMetadata.name}` };
 };
 
 export const convertFakesToResponses = (fakeDumpsMetadata: IDumpMetadata[]): DumpMetadataResponse[] => {
