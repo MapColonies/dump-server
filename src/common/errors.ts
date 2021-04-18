c7d5281d95b7c68841a546fe07103bb4723c289b
amito <samit2310@gmail.com>
2021-04-18 11:57:52 +0300

    feat(dump): added dump name uniqueness per bucket

--- src/common/errors.ts
+++ src/common/errors.ts
@@ -3,3 +3,7 @@ import { StatusCodes } from 'http-status-codes';
 export class NotFoundError extends Error {
   public status = StatusCodes.NOT_FOUND;
 }
+
+export class DumpNameAlreadyExistsError extends Error {
+  public status = StatusCodes.UNPROCESSABLE_ENTITY;
+}
